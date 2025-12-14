537fcd7e1906bfb3f7bf35cce9feb6ec02063c8d
Kim-sangjae <kimsangjae94114@gmail.com>
2025-12-14 18:12:26 +0900

    도커 , 연결전 커밋

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import { useAuthStore } from "@/stores/authStore";
 
 // ========== axios instance ==========
 const api = axios.create({
-    baseURL:"http://localhost:8081/api/v1",
+    baseURL:`${import.meta.env.VITE_API_BASE}/api/v1`,
     withCredentials: true,
 });
 
